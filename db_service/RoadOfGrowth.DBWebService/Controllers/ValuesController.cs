7ccfb34cd8afd195e91d0873aa5675e1177e8350
Joick <348606155@qq.com>
2019-10-17 18:24:02 +0800

    feat: 添加数据库访问项目

---
+++ db_service/RoadOfGrowth.DBWebService/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoadOfGrowth.DBRepository.Interface;
+using System.Collections.Generic;
+
+namespace RoadOfGrowth.DBWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static IUserService _userService;
+        public ValuesController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            string temp = _userService.GetUserAccount(1);
+            return new string[] { "value1", "value2", temp };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ db_service/RoadOfGrowth.DBWebService/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoadOfGrowth.DBWebService.Configuration;
+using System;
+
+namespace RoadOfGrowth.DBWebService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        private IContainer AutofacContainer;
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // 使用Autofac注入
+            ContainerBuilder builder = new ContainerBuilder();
+
+            // 注入数据库访接口
+            builder.RegisterModule<RepositoryModuleRegister>();
+
+            builder.Populate(services);
+            AutofacContainer = builder.Build();
+
+            return AutofacContainer.Resolve<IServiceProvider>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+
+            // 在程序停止时执行Autofac的释放函数
+            appLifetime.ApplicationStopped.Register(() => { AutofacContainer.Dispose(); });
+        }
+    }
+}
