11c20b46216f4f9452105dd110fc00bf3b5c1056
Joick <348606155@qq.com>
2019-10-22 18:03:44 +0800

    feat: 数据库访问项目添加RabbitMq支持

---
+++ db_service/RoadOfGrowth.DBUtility/RabbitMQUtility.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadOfGrowth.DBUtility
+{
+    /// <summary>
+    /// rabbitmq帮助类
+    /// </summary>
+    public static class RabbitMQUtility
+    {
+        private static readonly Dictionary<string, object> Config = ConfigUtility.GetSectionObj("RabbitMQConfig", "ErrorLog");
+        private static readonly string QueueName = Config["QueueName"].ToString();
+        private static readonly ConnectionFactory Factory = new ConnectionFactory()
+        {
+            HostName = Config["Host"].ToString()
+        };
+
+        /// <summary>
+        /// 入队
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pushData"></param>
+        public static async Task PushLog<T>(T pushData, string routingKey = "")
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (var connection = Factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(QueueName, true, false, false, null);
+
+                    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(pushData));
+                    var properties = channel.CreateBasicProperties();
+
+                    // 持久化消息
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: QueueName, basicProperties: properties, body: body);
+                }
+            });
+        }
+
+        /// <summary>
+        /// 入队
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pushData"></param>
+        public static async Task PushLog(string logData, string routingKey = "")
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (var connection = Factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(QueueName, false, false, false, null);
+
+                    var body = Encoding.UTF8.GetBytes(logData);
+                    var properties = channel.CreateBasicProperties();
+
+                    // 持久化消息
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: routingKey, basicProperties: properties, body: body);
+                }
+            });
+        }
+
+    }
+}
