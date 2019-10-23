a995298a192269294e1b29adc4269074007f259e
Joick <348606155@qq.com>
2019-10-23 17:52:34 +0800

    feat: 添加实体

---
+++ db_service/RoadOfGrowth.DBCommon/Entities/LogRequest.cs
@@ -0,0 +1,34 @@
+﻿using RoadOfGrowth.DBUtility.Providers.EntityExtension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoadOfGrowth.DBCommon.Entities
+{
+    public class LogRequest
+    {
+        [Column(Name = "id")]
+        public long Id { get; set; }
+
+        [Column(Name = "url")]
+        public string Url { get; set; }
+
+        [Column(Name = "methods")]
+        public string Method { get; set; }
+
+        [Column(Name = "request_body")]
+        public string RequestBody { get; set; }
+
+        [Column(Name = "request_time")]
+        public DateTime RequestTime { get; set; }
+
+        [Column(Name = "response_body")]
+        public string ResponseBody { get; set; }
+
+        [Column(Name = "response_time")]
+        public DateTime? ResponseTime { get; set; }
+
+        [Column(Name = "processing_time")]
+        public int ProcessingTime { get; set; }
+    }
+}
