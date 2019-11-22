28e44520fc9db6209a1b7d342dd15c0551626f9d
Joick <348606155@qq.com>
2019-11-22 16:48:15 +0800

    refactor: 调整代码风格,使用eslint规范

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -1,10 +1,14 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
+  //此项是用来告诉eslint找当前配置文件不能往父级查找
   root: true,
+  //此项是用来指定javaScript语言类型和风格，sourceType用来指定js导入的方式，默认是script，此处设置为module，指某块导入方式
   parserOptions: {
+    //此项是用来指定eslint解析器的，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
     parser: 'babel-eslint'
   },
+  //此项指定环境的全局变量，下面的配置指定为浏览器环境
   env: {
     browser: true,
   },
@@ -24,6 +28,18 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // js语句结尾必须使用分号
+    'semi': ['off', 'always'],
+    // 关键字后面使用一致的空格
+    'keyword-spacing': 0,
+    // 强制在 function的左括号之前使用一致的空格
+    'space-before-function-paren': 0,
+    // 引号类型
+    "quotes": [0, "single"],
+    // 禁止出现未使用过的变量
+    // 'no-unused-vars': 0,
+    // 要求或禁止末尾逗号
+    'comma-dangle': 0
   }
 }
